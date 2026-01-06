80d7030a8a782fa1286b96ea411c61df2a7fed3a
SAMBHAV JHA <sambhavjha@192.168.1.8>
2026-01-06 20:56:03 +0530

    kar do bhai

--- contest/codeforeces.cpp
+++ contest/codeforeces.cpp
@@ -3,31 +3,76 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     int t;
     cin >> t;
+
     while (t--) {
         int n;
         cin >> n;
-        vector<int> p(n);
-        for (int i = 0; i < n; ++i) cin >> p[i];
 
-        int l = 0, r = n - 1;
-        bool ok = true;
-        for (int x = 1; x <= n; ++x) {
-            if (l <= r && p[l] == x) {
-                ++l;
-            } else if (l <= r && p[r] == x) {
-                --r;
-            } else {
-                ok = false;
-                break;
+        vector<int> temp(n), temp1(n), temp2(n);
+        for (int i = 0; i < n; i++) cin >> temp[i];
+        for (int i = 0; i < n; i++) cin >> temp1[i];
+        for (int i = 0; i < n; i++) cin >> temp2[i];
+
+        vector<int> A(3*n), B(3*n), C(3*n);
+        for (int i = 0; i < 3*n; i++) {
+            A[i] = temp[i % n];
+            B[i] = temp1[i % n];
+            C[i] = temp2[i % n];
+        }
+
+        vector<vector<char>> ab(n, vector<char>(n, 0));
+        vector<vector<char>> bc(n, vector<char>(n, 0));
+
+        for (int j = 0; j < n; j++) {
+            vector<int> bad(3*n, 0);
+            for (int i = 0; i < 3*n; i++) {
+                if (A[i] >= B[i + j]) bad[i] = 1;
+            }
+
+            vector<int> pref(3*n + 1, 0);
+            for (int i = 0; i < 3*n; i++)
+                pref[i + 1] = pref[i] + bad[i];
+
+            for (int i = 0; i < n; i++) {
+                if (pref[i + n] - pref[i] == 0)
+                    ab[i][j] = 1;
             }
         }
 
-        cout << (ok ? "YES\n" : "NO\n");
+        for (int k = 0; k < n; k++) {
+            vector<int> bad(3*n, 0);
+            for (int j = 0; j < 3*n; j++) {
+                if (B[j] >= C[j + k]) bad[j] = 1;
+            }
+
+            vector<int> pref(3*n + 1, 0);
+            for (int j = 0; j < 3*n; j++)
+                pref[j + 1] = pref[j] + bad[j];
+
+            for (int j = 0; j < n; j++) {
+                if (pref[j + n] - pref[j] == 0)
+                    bc[j][k] = 1;
+            }
+        }
+
+        long long ans = 0;
+
+        for (int j = 0; j < n; j++) {
+            long long cnt1 = 0, cnt2 = 0;
+
+            for (int i = 0; i < n; i++)
+                if (ab[i][j]) cnt1++;
+
+            for (int k = 0; k < n; k++)
+                if (bc[j][k]) cnt2++;
+
+            ans += cnt1 * cnt2;
+        }
+
+        cout << ans << "\n";
     }
+
     return 0;
 }
