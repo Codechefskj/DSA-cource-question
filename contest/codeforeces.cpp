d466918166c6b564ce5bcfe7727e02a275a2b614
SAMBHAV JHA <sambhavjha57@gmail.com>
2026-06-03 02:10:56 +0530

    one of final

--- contest/codeforeces.cpp
+++ contest/codeforeces.cpp
@@ -1,66 +1,44 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdint>
+#include<unordered_map>
+#include<unordered_set>
+#include<set>
+#include<queue>
 using namespace std;
 
-class TrieNode{
-public:
-vector<TrieNode*> temp = vector<TrieNode*>(26, nullptr);
-bool flag = false ;
-};
+void prim(vector<vector<pair<int,int>>>& adj,vector<bool>& ismst,int src,int& ans){
+    priority_queue<pair<int,int>, vector<pair<int,int>> , vector<pair<int,int>>> q ;
+    q.push({0 , src});  // weight , node
+    while(!q.empty()){
+        int wt = q.top().first ; 
+        int node = q.top().second ; 
 
-class Trie{
-    TrieNode* root ; 
-    public : 
-    Trie(){
-        root = new TrieNode;
-    }
-
-    void insert(string word){
-       TrieNode* node = root ; 
-       for(int i = 0 ; i< word.size(); i++){
-        if(node->temp[word[i]-'a']){
-         node = node->temp[word[i]-'a'];
-        }
-        else{
-            node->temp[word[i]-'a'] = new TrieNode;
-            node = node->temp[word[i]-'a'];
-        }
-       }
-       node->flag= true ; 
-    }
-
-    bool search(string word){
-        TrieNode* node = root ; 
-        for(int i = 0 ; i< word.size(); i++){
-           if(node->temp[word[i]-'a'] != nullptr){
-             node = node->temp[word[i]-'a'];
-           }
-           else{
-            return false ;
-           }
-        }
-        return node->flag;
-    }
-
-    void remove(string word){
-    TrieNode* node = root;
-    for(int i = 0; i < word.size(); i++){
-        if(node->temp[word[i] - 'a'] != nullptr){
-            node = node->temp[word[i] - 'a'];
+        ismst[node]= true ; 
+        ans += wt ; 
+        for(auto x : adj[node]){
+            int adjnode = x.first;
+            int cost = x.second;
+            if(ismst[adjnode]== false){
+                q.push({cost,adjnode});
+            }
         }
     }
-    node->flag = false;
 }
 
-};
-
-
-int main() {
-Trie h ; 
-h.insert("apple");
-h.insert("hell");
-h.insert("car");
-h.insert("cars");
-cout << h.search("carsss")<<endl;
-cout << h.search("car")<<endl;
-cout << h.search("apple")<<endl;
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<vector<pair<int,int>>> adj(n);
+    for(int i = 0; i < m; i++){
+        int u, v, wt;
+        cin >> u >> v >> wt;
+        adj[u].push_back({v, wt});
+        adj[v].push_back({u, wt});
+    }
+    vector<bool> ismst(n, false);
+    int ans = 0;
+    prim(adj, ismst, 0, ans);
+    cout << ans << endl;
 }
